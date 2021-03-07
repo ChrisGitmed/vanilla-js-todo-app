efeb13b396049d864965453e7c05a87b1e2aeef4
Chris Gitmed <cgitmed@gmail.com>
2021-03-06 19:56:52 -0800

    Make it so that the todos are saved to local storage beforeunload, and appended to the DOM when DOMContentLoaded.

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
+let data = {
+    todos: []
+};
+
 const $form = document.querySelector('form');
 const $todoList = document.querySelector('ul');
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    const todoList = localStorage.getItem('todos');
+    if (todoList) {
+        data = JSON.parse(todoList);
+    }
+    if (data.todos.length) {
+        $todoList.hidden = false;
+        for (let i = 0; i < data.todos.length; i++) {
+            const $listItem = document.createElement('li');
+            $listItem.textContent = data.todos[i];
+            $todoList.appendChild($listItem);
+        }
+    }
+})
+
+window.addEventListener('beforeunload', (event) => {
+    localStorage.setItem('todos', JSON.stringify(data))
+})
+
 $form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const todo = $form.todo.value;
+    const todo = $form.elements.todo.value;
     if (todo) {
+        data.todos.push(todo)
         const $listItem = document.createElement('li');
         $listItem.textContent = todo;
         $todoList.appendChild($listItem);
